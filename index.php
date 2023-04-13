6714e2b47abd680222c283ba01197479e5cb5a98
Charanjeet Singh <charanjeets036@gmail.com>
2023-04-13 09:37:07 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$con = mysqli_connect('localhost','root','123','netnote');
+
+if(!$con){
+    echo mysqli_error($con);
+    exit;
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Net Notepad</title>
+</head>
+<body>
+<h1>Share data from devices on same network</h1>
+<div class="container">
+
+</div>
+</body>
+</html>
